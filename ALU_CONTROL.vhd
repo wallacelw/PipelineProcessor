02bbb8b06952bd7c12af996c7c6c7fb9c909f89d
wallacelw <70167391+wallacelw@users.noreply.github.com>
2023-02-14 19:34:55 -0300

    alu control almost done

--- ALU_CONTROL.vhd
+++ ALU_CONTROL.vhd
@@ -7,16 +7,80 @@ use IEEE.numeric_std.all;
 
 entity ALU_Control is
 	port (
-  		instr_in : in std_logic_vector(31 downto 0);
-        alu_op : in std_logic_vector(1 downto 0);
-        s : out std_logic_vector(31 downto 0)
+  		ALU_Control_instr : in std_logic_vector(31 downto 0);
+        ALU_Control_alu_op : in std_logic_vector(1 downto 0);
+        ALU_Control_out : out std_logic_vector(31 downto 0)
  	);
 end entity ALU_Control;
 
 architecture df of ALU_Control is
     
+    signal funct3 : std_logic_vector(3 downto 0);
+    signal funct7 : std_logic_vector(7 downto 0);
+
 begin
 
-    s <= std_logic_vector(signed(a) + signed(b));
+    funct3 <= resize(unsigned('0' & instr(14 downto 12)), 4);
+    funct7 <= resize(unsigned('0' & instr(31 downto 25)), 8);
 
+    process(ALU_Control_alu_op, ALU_Control_instr) begin
+        -- for LW, SW
+        if (ALU_Control_alu_op = "00") then 
+            ALU_Control_out <= "0000"; -- ADD 
+
+        -- for BRANCHES (SB)
+        elsif (ALU_Control_alu_op = "01") then 
+            if    (funct3 = x"0") then -- BEQ
+                ALU_Control_out <= "1100"; -- SEQ
+
+            elsif (funct3 = x"1") then -- BNE
+                ALU_Control_out <= "1101"; -- SEQ
+            
+            elsif (funct3 = x"4") then -- BLT
+                ALU_Control_out <= "1000"; -- SLT
+
+            elsif (funct3 = x"5") then -- BGE
+                ALU_Control_out <= "1010"; -- SGE
+
+            elsif (funct3 = x"6") then -- BLTU
+                ALU_Control_out <= "1001"; -- SLTU
+
+            elsif (funct3 = x"7") then -- BGEU
+                ALU_Control_out <= "1011"; -- SGEU
+
+        -- for R type and I type
+        elsif (ALU_Control_alu_op = "10") then 
+            if    (funct3 = x"0" and funct7 = x"00") then
+                ALU_Control_out <= "0000"; -- ADD
+
+            elsif (funct3 = x"0" and funct7 = x"20") then
+                ALU_Control_out <= "0001"; -- SUB
+
+            elsif (funct3 = x"7" and funct7 = x"00") then
+                ALU_Control_out <= "0010"; -- AND
+            
+            elsif (funct3 = x"6" and funct7 = x"00") then
+                ALU_Control_out <= "0011"; -- OR
+
+            elsif (funct3 = x"4" and funct7 = x"00") then
+                ALU_Control_out <= "0100"; -- XOR
+
+            elsif (funct3 = x"1" and funct7 = x"00") then
+                ALU_Control_out <= "0101"; -- SLL
+
+            elsif (funct3 = x"5" and funct7 = x"00") then
+                ALU_Control_out <= "0110"; -- SRL
+
+            elsif (funct3 = x"5" and funct7 = x"20") then
+                ALU_Control_out <= "0111"; -- SRA
+
+            elsif (funct3 = x"2"                   ) then
+                ALU_Control_out <= "1000"; -- SLT
+
+            elsif (funct3 = x"3"                   ) then
+                ALU_Control_out <= "1001"; -- SLTU
+
+            end if;
+        end if;
+    end process;
 end df;
--- CONTROL.vhd
+++ CONTROL.vhd
@@ -28,7 +28,6 @@ architecture df of CONTROL is
     signal opcode : std_logic_vector(7 downto 0);
     signal funct3 : std_logic_vector(3 downto 0);
     signal funct7 : std_logic_vector(7 downto 0);
-    signal controls : std_logic_vector(10 downto 0);
 
 begin
 
@@ -36,184 +35,176 @@ begin
     funct3 <= resize(unsigned('0' & instr(14 downto 12)), 4);
     funct7 <= resize(unsigned('0' & instr(31 downto 25)), 8);
 
-    process(opcode) begin
-        case opcode is
-            when "0000011" => -- lw
-                controls <= "10010010000";
-            when "0100011" => -- sw
-                controls <= "00111000000";
-            when "0110011" => -- R-Type
-                controls <= "1--00000100";
-            when "1100011" => -- beq
-                controls <= "01000001010";
-            when "0010011" => -- I-Type-ALU
-                controls <= "10010000100";
-            when "1101111" => -- jal
-                controls <= "11100100001";
-            when others => -- not valid
-                controls <= "-----------";
-        end case;
-    end process;
+    ---- Lógico-Aritméticas
 
-    (RegWrite, )
-    -- ---- Lógico-Aritméticas
-
-    -- -- ADD (R)
-    -- if (opcode = x"33" and funct3 = x"0" and funct7 = x"00") then
-    --     ALUSrc <= '0';
-    --     ALUOp <= "10";
-    --     Branch <= '0';
-    --     MemRead <= '0';
-    --     MemWrite <= '0';
-    --     RegWrite <= '1';
-    --     Mem2Reg <= '0';
+    -- ADD (R)
+    if (opcode = x"33" and funct3 = x"0" and funct7 = x"00") then
+        ALUSrc <= '0';
+        ALUOp <= "10";
+        Branch <= '0';
+        MemRead <= '0';
+        MemWrite <= '0';
+        RegWrite <= '1';
+        Mem2Reg <= '0';
     
-    -- -- ADDi 
-    -- elsif (opcode = x"13" and func3 = x"0") then
-    --     ALUSrc <= '1';
-    --     ALUOp <= "10";
-    --     Branch <= '0';
-    --     MemRead <= '0';
-    --     MemWrite <= '0';
-    --     RegWrite <= '1';
-    --     Mem2Reg <= '0';
+    -- ADDi (I)
+    elsif (opcode = x"13" and func3 = x"0") then
+        ALUSrc <= '1';
+        ALUOp <= "10";
+        Branch <= '0';
+        MemRead <= '0';
+        MemWrite <= '0';
+        RegWrite <= '1';
+        Mem2Reg <= '0';
 
-    -- -- SUB (R)
-    -- elsif (opcode = x"33" and funct3 = x"0" and funct7 = x"20") then
-    --     ALUSrc <= '0';
-    --     ALUOp <= "10";
-    --     Branch <= '0';
-    --     MemRead <= '0';
-    --     MemWrite <= '0';
-    --     RegWrite <= '1';
-    --     Mem2Reg <= '0';
+    -- SUB (R)
+    elsif (opcode = x"33" and funct3 = x"0" and funct7 = x"20") then
+        ALUSrc <= '0';
+        ALUOp <= "10";
+        Branch <= '0';
+        MemRead <= '0';
+        MemWrite <= '0';
+        RegWrite <= '1';
+        Mem2Reg <= '0';
 
-    -- -- AND (R)
-    -- elsif (opcode = x"33" and funct3 = x"7" and funct7 = x"00") then
-    --     ALUSrc <= '0';
-    --     ALUOp <= "10";
-    --     Branch <= '0';
-    --     MemRead <= '0';
-    --     MemWrite <= '0';
-    --     RegWrite <= '1';
-    --     Mem2Reg <= '0';
+    -- AND (R)
+    elsif (opcode = x"33" and funct3 = x"7" and funct7 = x"00") then
+        ALUSrc <= '0';
+        ALUOp <= "10";
+        Branch <= '0';
+        MemRead <= '0';
+        MemWrite <= '0';
+        RegWrite <= '1';
+        Mem2Reg <= '0';
         
-    -- -- ANDi
-    -- elsif (opcode = x"13" and func3 = x"7") then
+    -- ANDi
+    elsif (opcode = x"13" and func3 = x"7") then
 
-    -- -- LUI
-    -- elsif (opcode = x"37") then
+    -- LUI
+    elsif (opcode = x"37") then
 
-    -- -- SLT (R)
-    -- elsif (opcode = x"33" and funct3 = x"2" and funct7 = x"00") then
-    --     ALUSrc <= '0';
-    --     ALUOp <= "10";
-    --     Branch <= '0';
-    --     MemRead <= '0';
-    --     MemWrite <= '0';
-    --     RegWrite <= '1';
-    --     Mem2Reg <= '0';
+    -- SLT (R)
+    elsif (opcode = x"33" and funct3 = x"2" and funct7 = x"00") then
+        ALUSrc <= '0';
+        ALUOp <= "10";
+        Branch <= '0';
+        MemRead <= '0';
+        MemWrite <= '0';
+        RegWrite <= '1';
+        Mem2Reg <= '0';
         
-    -- -- OR (R)
-    -- elsif (opcode = x"33" and funct3 = x"6" and funct7 = x"00") then
-    --     ALUSrc <= '0';
-    --     ALUOp <= "10";
-    --     Branch <= '0';
-    --     MemRead <= '0';
-    --     MemWrite <= '0';
-    --     RegWrite <= '1';
-    --     Mem2Reg <= '0';
+    -- OR (R)
+    elsif (opcode = x"33" and funct3 = x"6" and funct7 = x"00") then
+        ALUSrc <= '0';
+        ALUOp <= "10";
+        Branch <= '0';
+        MemRead <= '0';
+        MemWrite <= '0';
+        RegWrite <= '1';
+        Mem2Reg <= '0';
         
-    -- -- ORi
-    -- elsif (opcode = x"13" and func3 = x"6") then
+    -- ORi
+    elsif (opcode = x"13" and func3 = x"6") then
 
-    -- -- XOR (R)
-    -- elsif (opcode = x"33" and funct3 = x"4" and funct7 = x"00") then
-    --     ALUSrc <= '0';
-    --     ALUOp <= "10";
-    --     Branch <= '0';
-    --     MemRead <= '0';
-    --     MemWrite <= '0';
-    --     RegWrite <= '1';
-    --     Mem2Reg <= '0';
+    -- XOR (R)
+    elsif (opcode = x"33" and funct3 = x"4" and funct7 = x"00") then
+        ALUSrc <= '0';
+        ALUOp <= "10";
+        Branch <= '0';
+        MemRead <= '0';
+        MemWrite <= '0';
+        RegWrite <= '1';
+        Mem2Reg <= '0';
         
-    -- -- XORi
-    -- elsif (opcode = x"13" and func3 = x"4") then
+    -- XORi
+    elsif (opcode = x"13" and func3 = x"4") then
 
-    -- -- SLLi
-    -- elsif (opcode = x"13" and funct3 = x"1" and funct7 = x"00") then
+    -- SLLi
+    elsif (opcode = x"13" and funct3 = x"1" and funct7 = x"00") then
 
-    -- -- SRLi
-    -- elsif (opcode = x"13" and funct3 = x"5" and funct7 = x"00") then
+    -- SRLi
+    elsif (opcode = x"13" and funct3 = x"5" and funct7 = x"00") then
 
-    -- -- SRAi
-    -- elsif (opcode = x"13" and funct3 = x"5" and funct7 = x"20") then
+    -- SRAi
+    elsif (opcode = x"13" and funct3 = x"5" and funct7 = x"20") then
 
-    -- -- SLTi
-    -- elsif (opcode = x"13" and funct3 = x"2") then
+    -- SLTi
+    elsif (opcode = x"13" and funct3 = x"2") then
 
-    -- -- SLTU (R)
-    -- elsif (opcode = x"33" and funct3 = x"3" and funct7 = x"00") then
-    --     ALUSrc <= '0';
-    --     ALUOp <= "10";
-    --     Branch <= '0';
-    --     MemRead <= '0';
-    --     MemWrite <= '0';
-    --     RegWrite <= '1';
-    --     Mem2Reg <= '0';
+    -- SLTU (R)
+    elsif (opcode = x"33" and funct3 = x"3" and funct7 = x"00") then
+        ALUSrc <= '0';
+        ALUOp <= "10";
+        Branch <= '0';
+        MemRead <= '0';
+        MemWrite <= '0';
+        RegWrite <= '1';
+        Mem2Reg <= '0';
         
-    -- -- SLTUi
-    -- elsif (opcode = x"13" and funct3 = x"3") then
+    -- SLTUi
+    elsif (opcode = x"13" and funct3 = x"3") then
 
-    -- -- AUIPC
-    -- elsif (opcode = x"17") then
-    --     ALUSrc <= '0';
-    --     ALUOp <= "10";
-    --     Branch <= '0';
-    --     MemRead <= '0';
-    --     MemWrite <= '0';
-    --     RegWrite <= '1';
-    --     Mem2Reg <= '0';
+    -- AUIPC
+    elsif (opcode = x"17") then
+        ALUSrc <= '0';
+        ALUOp <= "10";
+        Branch <= '0';
+        MemRead <= '0';
+        MemWrite <= '0';
+        RegWrite <= '1';
+        Mem2Reg <= '0';
     
-    -- ---- Subrotinas
+    ---- Subrotinas
 
-    -- -- JAL (UJ) 
-    -- else if (opcode = x"6f") then
+    -- JAL (UJ) 
+    else if (opcode = x"6f") then
 
-    -- -- JALR (I)
-    -- else if (opcode = x"67" and funct3 = x"0") then
+    -- JALR (I)
+    else if (opcode = x"67" and funct3 = x"0") then
 
-    -- ---- Saltos
+    ---- Saltos
 
-    -- -- BEQ (SB)
-    -- else if (opcode = x"63" and funct3 = x"0") then
+    -- BEQ (SB)
+    else if (opcode = x"63" and funct3 = x"0") then
 
-    -- -- BNE (SB)
-    -- else if (opcode = x"63" and funct3 = x"1") then
+    -- BNE (SB)
+    else if (opcode = x"63" and funct3 = x"1") then
 
-    -- -- BLT (SB)
-    -- else if (opcode = x"63" and funct3 = x"4") then
+    -- BLT (SB)
+    else if (opcode = x"63" and funct3 = x"4") then
 
-    -- -- BGE (SB)
-    -- else if (opcode = x"63" and funct3 = x"5") then
+    -- BGE (SB)
+    else if (opcode = x"63" and funct3 = x"5") then
 
-    -- -- BGEU (SB)
-    -- else if (opcode = x"63" and funct3 = x"7") then
+    -- BGEU (SB)
+    else if (opcode = x"63" and funct3 = x"7") then
 
-    -- -- BLTU (SB)
-    -- else if (opcode = x"63" and funct3 = x"6") then
+    -- BLTU (SB)
+    else if (opcode = x"63" and funct3 = x"6") then
 
-    -- ---- Memória
+    ---- Memória
 
-    -- -- LW (I)
-    -- else if (opcode = x"03" and func3 = x"2") then
+    -- LW (I)
+    else if (opcode = x"03" and func3 = x"2") then
+        ALUSrc <= '1';
+        ALUOp <= "00";
+        Branch <= '0';
+        MemRead <= '1';
+        MemWrite <= '0';
+        RegWrite <= '1';
+        Mem2Reg <= '1';
+    -- SW (S)
+    else if (opcode = x"23" and func3 = x"2") then
+        ALUSrc <= '1';
+        ALUOp <= "00";
+        Branch <= '0';
+        MemRead <= '0';
+        MemWrite <= '1';
+        RegWrite <= '0';
+        Mem2Reg <= '-';
+    ---- NOP
+    else
 
-    -- -- SW (S)
-    -- else if (opcode = x"23" and func3 = x"2") then
-
-    -- ---- NOP
-    -- else
-
-    -- end if;
+    end if;
 
 end df;
